3adbd3ae2a1a12f9d7ccef9cac994cff7020419e
Marco <marcogarcia831@gmail.com>
2025-09-19 14:43:48 -0700

    got calendar to run and added api keys to .env file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { gapi } from "gapi-script";
+
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 
 function App() {
+  const [events, setEvents] = useState([]);
+
+ useEffect(() => {
+  const initClient = () => {
+    gapi.client
+      .init({
+        apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        discoveryDocs: [
+          "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+        ],
+        scope: "https://www.googleapis.com/auth/calendar.readonly",
+      })
+      .then(() => {
+        const auth = gapi.auth2.getAuthInstance();
+        if (!auth.isSignedIn.get()) {
+          auth.signIn();
+        }
+        loadEvents();
+      })
+      .catch((err) => {
+        console.error("❌ GAPI init error:", err);
+      });
+  };
+
+  gapi.load("client:auth2", initClient);
+}, []);
+
+
+  const loadEvents = () => {
+    gapi.client.calendar.events
+      .list({
+        calendarId: "primary",
+        timeMin: new Date().toISOString(),
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 10,
+        orderBy: "startTime",
+      })
+      .then((response) => {
+        const items = response.result.items;
+        const mappedEvents = items.map((event) => ({
+          title: event.summary,
+          start: event.start.dateTime || event.start.date,
+          end: event.end.dateTime || event.end.date,
+        }));
+        setEvents(mappedEvents);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I love PP
-        </p>
-        
-      
-      </header>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        height="100vh"
+        events={events}
+      />
     </div>
   );
 }
